ba25b840d2da5b8211edd90e0c636fe22c4c9abb
levi_lydia <levi_lydia@appacademy.io>
2015-02-23 18:13:02 -0500

    add ship

--- arguments.js
+++ arguments.js
@@ -18,8 +18,8 @@ Function.prototype.myBind = function(context) {
 }
 
 function Cat(name, age) {
-this.name = name;
-this.age = age;
+  this.name = name;
+  this.age = age;
 }
 
 Cat.prototype.meow = function(name) {
@@ -37,4 +37,106 @@ sennacy = new Cat("sennacy", 12);
 
 // console.log(sennacy.meow(1,2,3,5,7));
 
-setTimeout(sennacy.meow.myBind(sennacy, 1,2,3,4,5),1000);
+// setTimeout(sennacy.meow.myBind(sennacy, 1,2,3,4,5),1000);
+
+// var curriedSum = function(num) {
+//   var times = arguments[0];
+//   var args = [].slice.call(arguments, 1);
+//   var allArgs = arguments;
+//   // if args === num + 1 retrun function with args already set
+//   if (args.length === num) {
+//     var sum = 0;
+//     args.forEach(function(el) {
+//       sum += el;
+//     });
+//     return sum;
+//   } else {
+//     return (function (num) {
+//       this.arguments = allArgs;
+//       console.log(this.arguments);
+//       arguments[arguments.length] = num;
+//       // return curriedSum;
+//     })
+//   };
+// }
+
+
+var curriedSum = function(numArgs){
+  var numbers = [];
+
+  var _curriedSum = function(num){
+    numbers.push(num);
+    if (numbers.length === numArgs){
+      var sum = 0;
+      numbers.forEach(function(el) {
+        sum += el;
+      });
+      console.log(sum)
+    } else {
+      return _curriedSum;
+    }
+  }
+
+  return _curriedSum;
+}
+
+
+
+var f1 = curriedSum(3);
+console.log(f1);
+var f2 = f1(4);
+console.log(f2);
+var f3 = f2(20);
+console.log(f1);
+var result = f3(6); // = 30
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
